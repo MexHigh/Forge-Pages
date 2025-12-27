446544397e23111c4b5ca2b84270aec4f92d91ad
Leon Schmidt <mail@leon.wtf>
2025-12-27 17:18:13 +0000

    chore: updated test scripts

--- test-delete.sh
+++ test-delete.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 curl -X DELETE "http://forge-pages.test:8080/deploy?repo=testuser/proj1"
+echo
+
+curl -X DELETE "http://forge-pages.test:8080/deploy?repo=testuser/proj1&additional_base_path=protected"
+echo
--- test-deploy.sh
+++ test-deploy.sh
@@ -6,9 +6,16 @@ TMPDIR=$(mktemp -d)
 
 echo "<html><head><link rel='stylesheet' href='./styles.css'/></head><body><h1>It works!</h1></body></html>" > $TMPDIR/index.html
 echo "h1 { color: red; }" > $TMPDIR/styles.css
-tar -czvf content.tar.gz -C $TMPDIR .  
+mkdir $TMPDIR/protected
+echo "<html><head></head><body><h1>HTML in dir!</h1></body></html>" > $TMPDIR/protected/index.html
+tar -czvf content.tar.gz -C $TMPDIR .
+
+# deploy to __ROOT__
 curl --data-binary @content.tar.gz "http://forge-pages.test:8080/deploy?repo=testuser/proj1"
+echo
+
+# deploy to protected dir
+curl --data-binary @content.tar.gz "http://forge-pages.test:8080/deploy?repo=testuser/proj1&additional_base_path=protected&protect=1"
+echo
 
 rm -rf $TMPDIR content.tar.gz
-
-#echo "Done! Page should be available at http://testuser.forge-pages.test:8080/proj1/"
